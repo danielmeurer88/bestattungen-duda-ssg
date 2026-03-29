1a6113c814440d250a048a4e8b432547bbe9e5d9
danielmeurer88 <danielmeurer@gmx.net>
2026-03-29 19:05:26 +0200

    manual scrolling for the carousel implemented

--- src/app/shared/carousel/carousel.ts
+++ src/app/shared/carousel/carousel.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, inject, Input, OnDestroy, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { UtilSwipeDirective } from '../swipe/swipe.directive';
+import { UtilGestureService } from '../swipe/gesture.service';
 
 @Component({
   selector: 'app-carousel',
   imports: [
-    CommonModule,
-    UtilSwipeDirective
+    CommonModule
   ],
   templateUrl: './carousel.html',
   styleUrl: './carousel.scss',
@@ -15,12 +15,18 @@ import { UtilSwipeDirective } from '../swipe/swipe.directive';
 export class Carousel implements AfterViewInit, OnDestroy {
 
   private elementRef = inject(ElementRef);
+  private utilGestureService = inject(UtilGestureService);
 
   /**
    * manualScrolling not yet implemented - work with IntersectionObserver to update index
    * (manualScrolling = javascriptless / html scrolling like using scroll wheel or the scroll bar)
    */
-  protected manualScrolling = false;
+  protected manualScrolling = true;
+
+  /**
+   * swipe gestures is only used if manual scrolling is deactivated
+   */
+  private swipeSubscription: Subscription = null!;
 
   @Input() images: Array<{
     src: string;
@@ -29,9 +35,33 @@ export class Carousel implements AfterViewInit, OnDestroy {
 
   index$ = new BehaviorSubject<number>(0);
 
-  ngAfterViewInit(): void {}
+  ngAfterViewInit(): void {
 
-  ngOnDestroy(): void {}
+    const carousel = this.getCarouselSlideElement();
+    carousel.addEventListener('scroll', this.onScroll, {capture: true});
+
+    if (!this.manualScrolling) {
+
+      const containerEl = (this.elementRef.nativeElement as HTMLElement).querySelector('.carousel-container') as HTMLElement;
+      this.swipeSubscription = this.utilGestureService.getGesturesFrom(containerEl).subscribe(gestures => {
+        if (gestures.gesture === 'swipeLeft') {
+          this.nextSlide();
+        }
+
+        if (gestures.gesture === 'swipeRight') {
+          this.prevSlide();
+        }
+      });
+    }
+
+  }
+
+
+  ngOnDestroy(): void {
+    this.swipeSubscription?.unsubscribe();
+    const carousel = this.getCarouselSlideElement();
+    carousel.removeEventListener('scroll', this.onScroll, {capture: true});
+  }
 
   prevSlide() {
     this.jumpTo(this.index$.value - 1);
@@ -42,12 +72,12 @@ export class Carousel implements AfterViewInit, OnDestroy {
   }
 
   jumpTo(num: number) {
-    num = this.updateCurrentIndex(num);
+    num = this.getCurrentIndex(num);
     const slideEl = this.getSlideElement(num);
     slideEl.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'nearest'});
   }
 
-  private updateCurrentIndex(wantedIndex: number = 0): number {
+  private getCurrentIndex(wantedIndex: number = 0): number {
 
     const maxi = this.images.length - 1;
 
@@ -59,17 +89,22 @@ export class Carousel implements AfterViewInit, OnDestroy {
       wantedIndex = maxi;
     }
 
-    this.index$.next(wantedIndex);
     return wantedIndex;
 
   }
 
-  private getSlideElement(i: number) {
-    return this.getAllSlides()[i];
+  private onScroll = (e: Event) => {
+    const left = (e.target as HTMLElement).scrollLeft;
+    const length = (e.target as HTMLElement).getBoundingClientRect().width;
+
+    const index = Math.round(left / length);
+
+    this.index$.next(index);
   }
 
-  private getAllSlides() {
-    return Array.from((this.elementRef.nativeElement as HTMLElement).querySelectorAll('.carousel-slides .carousel-slide')) as HTMLElement[];
+  private getSlideElement(i: number) {
+    const allSlides = Array.from((this.elementRef.nativeElement as HTMLElement).querySelectorAll('.carousel-slides .carousel-slide')) as HTMLElement[];
+    return allSlides[i];
   }
 
   private getCarouselSlideElement() {
